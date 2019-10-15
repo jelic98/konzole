21a03bf02e849ded19ea8b07dbca5712a0413090
Lazar Jelic <lazarjelic98@gmail.com>
2019-10-15 22:27:30 +0200

    Added another Input constructor which accepts key and title

--- framework/src/main/java/com/konzole/framework/Input.java
+++ framework/src/main/java/com/konzole/framework/Input.java
@@ -5,14 +5,29 @@ package com.konzole.framework;
  */
 public class Input {
 
-    private String title, value;
+    private String key, value, title;
 
     /**
-     * One and only constructor.
+     * Basic constructor that accepts just an input key which is copied to title.
+     * @param key Input key which is user to fetch input value.
+     */
+    public Input(String key) {
+        this.key = key;
+        this.title = key;
+    }
+
+    /**
+     * Another constructor that accepts an input key and an input title.
+     * @param key Input key which is user to fetch input value.
      * @param title Input title which is displayed to the user.
      */
-    public Input(String title) {
+    public Input(String key, String title) {
+        this.key = key;
         this.title = title;
+    }
+
+    String getKey() {
+        return key;
     }
 
     String getTitle() {
